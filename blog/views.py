aa22b3d5be4e26faea56eabf76a150fc09bbc67f
Seminar Trainer <trainer@example.com>
2026-01-03 13:57:07 +0000

    Block 3: URL-Routing + Views (GET/POST/JSON, CBV)

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,20 @@
 
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from django.views import View
 
 def home(request):
     return HttpResponse("Hello Django")
+
+class PingView(View):
+    def get(self, request):
+        return JsonResponse({"status": "ok"})
+
+def echo(request):
+    msg = request.GET.get("msg", "")
+    return JsonResponse({"echo": msg})
+
+def submit(request):
+    if request.method != "POST":
+        return JsonResponse({"detail": "Method not allowed"}, status=405)
+    name = request.POST.get("name", "")
+    return JsonResponse({"received": name})
