5b288d37103a6c3f022dabc3fa3a35648e2557d9
Seminar Trainer <trainer@example.com>
2026-01-03 13:57:20 +0000

    Block 4: Templates (base/home/about) + About-View

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("about/", views.about, name="about"),
     path("ping/", views.PingView.as_view(), name="ping"),
     path("echo/", views.echo, name="echo"),
     path("submit/", views.submit, name="submit"),
