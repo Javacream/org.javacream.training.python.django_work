ccbe5c3c5810b6fb054d131751675876dee90ee1
Seminar Trainer <trainer@example.com>
2026-01-03 13:57:07 +0000

    Block 3: URL-Routing + Views (GET/POST/JSON, CBV)

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("ping/", views.PingView.as_view(), name="ping"),
+    path("echo/", views.echo, name="echo"),
+    path("submit/", views.submit, name="submit"),
 ]
