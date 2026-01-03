3127eb9fd81568bd969102f0371550acf52075d3
Seminar Trainer <trainer@example.com>
2026-01-03 13:58:06 +0000

    Block 9: Auth (login_required) + Staff-Delete + Auth-URLs

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,14 @@
 
 import logging
+from django.contrib.admin.views.decorators import staff_member_required
+from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
 from django.views import View
 
 from .forms import PostForm
+from .models import Post
 
 logger = logging.getLogger(__name__)
 
@@ -29,14 +32,23 @@ def submit(request):
     name = request.POST.get("name", "")
     return JsonResponse({"received": name})
 
+@login_required
 def post_create(request):
     if request.method == "POST":
         form = PostForm(request.POST)
         if form.is_valid():
             post = form.save()
             logger.info("Post created: id=%s title=%s", post.id, post.title)
-            return redirect(reverse("home"))  # PRG
+            return redirect(reverse("home"))
     else:
         form = PostForm()
-
     return render(request, "blog/post_form.html", {"form": form})
+
+@staff_member_required
+def post_delete(request, pk: int):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        logger.warning("Post deleted: id=%s title=%s", post.id, post.title)
+        post.delete()
+        return redirect(reverse("home"))
+    return render(request, "blog/post_confirm_delete.html", {"post": post})
